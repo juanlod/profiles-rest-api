e1bc0f73b9397fcada3c5436a742378ccf41a8c0
Jaunos <juancorvelle@gmail.com>
2020-01-19 13:42:22 +0100

    Add profiles viewset

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -6,6 +6,8 @@ from profiles_api import views
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name= 'hello-viewset')
+# Si solo es un conjunto de consultas, no es necesario poner el base_name. 
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
     path('hellow-view/', views.HelloApiView.as_view()),
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -4,6 +4,13 @@ from rest_framework import status
 from rest_framework import viewsets
 
 from profiles_api import serializers
+from profiles_api import models
+
+# Permisos
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+            
+
 
 
 class HelloApiView(APIView):
@@ -90,3 +97,13 @@ class HelloViewSet(viewsets.ViewSet):
     def destroy(self, request, pk=None):
         """Handle removing an object"""
         return Response({'http_method': 'DELETE'})
+    
+    
+class UserProfileViewSet(viewsets.ModelViewSet):
+    """Handle creating and updating profiles"""
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    # Se crea tupla de un solo elemento
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile,) 
+    
